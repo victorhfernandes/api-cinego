7f95a121f53e532abc34a8b8ef87cd8f86590c2b
Victor Hugo Fernandes <victorh.fernandes.o@gmail.com>
2023-05-15 02:41:14 -0300

    objeto horarios para array

--- scraping/scraping.js
+++ scraping/scraping.js
@@ -1,126 +1,125 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
-const converter = require('./conversao-dados');
-const { getFilmesEmBreve } = require('./em-breve')
+const converter = require("./conversao-dados");
+const { getFilmesEmBreve } = require("./em-breve");
 
 exports.sessoesScraping = async (nmCinema) => {
-    do {
-        var axiosResponse = await axios.request({
-            method: "GET",
-            url: "https://www.google.com/search?q=" + nmCinema,
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
-            }
-        });
-    } while (axiosResponse.length === 0)
+  do {
+    var axiosResponse = await axios.request({
+      method: "GET",
+      url: "https://www.google.com/search?q=" + nmCinema,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+      },
+    });
+  } while (axiosResponse.length === 0);
 
-    const $ = cheerio.load(axiosResponse.data);
+  const $ = cheerio.load(axiosResponse.data);
 
-    var arrayFilmes = [],
-        arraySessoes = [],
-        arrayHorarios = [],
-        arrayEmBreve = [],
-        sessoesCinema = [],
-        linguagens = [],
-        dataSessao,
-        tecnologia,
-        linguagem,
-        nomeFilme,
-        horario,
-        i = 0,
-        j = 0;
+  var arrayFilmes = [],
+    arraySessoes = [],
+    arrayHorarios = [],
+    arrayEmBreve = [],
+    sessoesCinema = [],
+    linguagens = [],
+    dataSessao,
+    tecnologia,
+    linguagem,
+    nomeFilme,
+    horario,
+    i = 0,
+    j = 0;
 
-    for (const element of $('.WIDPrb')) { //:first
-        dataSessao = $(element).attr('data-date');
-        dataSessao = converter.data(dataSessao, j);
-        //console.log(dataSessao);
-        j++;
-        let lr_c_fcb = $(element).find('.lr_c_fcb'); //:eq(1)
-        for (const element of lr_c_fcb) {
-            nomeFilme = $(element).attr('data-movie-name');
-            //console.log(nomeFilme);
-            let YHR1ce = $(element).find('.YHR1ce');
-            for (const element of YHR1ce) {
-                linguagens[i] = $(element).text();
-                //console.log(linguagens[i]);
-                i++;
-            }
-            let lr_c_fcc = $(element).find('.lr_c_fcc');
-            i = 0;
-            for (const element of lr_c_fcc) {
-                let lr_c_vn = $(element).find('.lr_c_vn');
-                for (const element of lr_c_vn) {
-                    tecnologia = $(element).text();
-                    tecnologia = converter.tecnologia(tecnologia);
-                    //console.log(tecnologia);
-                    linguagem = linguagens[i];
-                    linguagem = converter.linguagem(linguagem);
-                    //console.log(linguagem + ' ' + i);
-                    i++;
-                }
-                let std = $(element).find('.std-ts');
-                for (const element of std) {
-                    horario = $(element).text();
-                    horario = converter.horario(horario);
-                    //console.log(horario);
-
-                    const objHorario = {
-                        inicio: horario
-                    }
-
-                    arrayHorarios.push(objHorario);
-                }
-
-                const objSessao = {
-                    linguagem: linguagem,
-                    tecnologia: tecnologia,
-                    horarios: arrayHorarios
-                }
-
-                arraySessoes.push(objSessao);
-
-                //console.log(arrayHorarios);
-                arrayHorarios = [];
-            }
-            //console.log(arraySessoes);
-
-            const objFilme = {
-                nome: nomeFilme,
-                sessoes: arraySessoes,
-            }
-            
-            arrayFilmes.push(objFilme);
-
-            arraySessoes = [];
-            linguagens = [];
-            i = 0;
+  for (const element of $(".WIDPrb")) {
+    //:first
+    dataSessao = $(element).attr("data-date");
+    dataSessao = converter.data(dataSessao, j);
+    //console.log(dataSessao);
+    j++;
+    let lr_c_fcb = $(element).find(".lr_c_fcb"); //:eq(1)
+    for (const element of lr_c_fcb) {
+      nomeFilme = $(element).attr("data-movie-name");
+      //console.log(nomeFilme);
+      let YHR1ce = $(element).find(".YHR1ce");
+      for (const element of YHR1ce) {
+        linguagens[i] = $(element).text();
+        //console.log(linguagens[i]);
+        i++;
+      }
+      let lr_c_fcc = $(element).find(".lr_c_fcc");
+      i = 0;
+      for (const element of lr_c_fcc) {
+        let lr_c_vn = $(element).find(".lr_c_vn");
+        for (const element of lr_c_vn) {
+          tecnologia = $(element).text();
+          tecnologia = converter.tecnologia(tecnologia);
+          //console.log(tecnologia);
+          linguagem = linguagens[i];
+          linguagem = converter.linguagem(linguagem);
+          //console.log(linguagem + ' ' + i);
+          i++;
         }
-        //console.log(arrayFilmes);
-
-        const objData = {
-            data: dataSessao,
-            filmes: arrayFilmes
+        let std = $(element).find(".std-ts");
+        for (const element of std) {
+          horario = $(element).text();
+          horario = converter.horario(horario);
+          //console.log(horario);
+          arrayHorarios.push(horario);
         }
 
-        sessoesCinema.push(objData);
+        const objSessao = {
+          linguagem: linguagem,
+          tecnologia: tecnologia,
+          horarios: arrayHorarios,
+        };
 
-        arrayFilmes = [];
-    }
-    const diff = getFilmesEmBreve(sessoesCinema);
+        arraySessoes.push(objSessao);
 
-    for (let filme of diff){
-        if(filme.length === 0){continue;};
-        const objNome = {
-            nome: filme.toString()
-        }
-        arrayEmBreve.push(objNome);
-    }
-    const objEmBreve = {
-        emBreve: arrayEmBreve
-    }
-    sessoesCinema.push(objEmBreve);
+        //console.log(arrayHorarios);
+        arrayHorarios = [];
+      }
+      //console.log(arraySessoes);
 
-    return sessoesCinema;
-}
+      const objFilme = {
+        nome: nomeFilme,
+        sessoes: arraySessoes,
+      };
+
+      arrayFilmes.push(objFilme);
+
+      arraySessoes = [];
+      linguagens = [];
+      i = 0;
+    }
+    //console.log(arrayFilmes);
+
+    const objData = {
+      data: dataSessao,
+      filmes: arrayFilmes,
+    };
+
+    sessoesCinema.push(objData);
+
+    arrayFilmes = [];
+  }
+  const diff = getFilmesEmBreve(sessoesCinema);
+
+  for (let filme of diff) {
+    if (filme.length === 0) {
+      continue;
+    }
+    const objNome = {
+      nome: filme.toString(),
+    };
+    arrayEmBreve.push(objNome);
+  }
+  const objEmBreve = {
+    emBreve: arrayEmBreve,
+  };
+  sessoesCinema.push(objEmBreve);
+
+  return sessoesCinema;
+};
 
 //sessoesScraping('Cinemark Praiamar');
